bb51726adcd7c3710a66f1b2e382a49ca16a7ea4
Youngki Kang <yellowgg2@gmail.com>
2022-04-05 04:43:09 +0900

    admin 사용자를 env로 받음

--- src/global-bot-config.ts
+++ src/global-bot-config.ts
@@ -2,30 +2,13 @@ import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const GUIDE_IDENTITY = "installGuide";
-export const LOG_LIST = "logList";
-export const ADMIN_CHATID = 22442558;
+export const ADMIN_CHATID = process.env.ADMIN_CHATID
+  ? parseInt(process.env.ADMIN_CHATID)
+  : 22442558;
 export const NON_AUTH_WARN_MSG = "🌼 권한이 없습니다.\n관리자에게 문의하세요.";
 
 export interface DynamicObject {
   [key: string]: any;
-}
-
-// inline button callback data
-// 최대 64byte가 넘지 못한다
-export interface IInlineKeyboardCallbackData {
-  type: string;
-  step: number;
-  guide_id: string;
-  companyCode?: string;
-  index?: string;
-}
-
-export interface IGuides {
-  guide_id: string;
-  step: number;
-  description: string;
-  title: string;
 }
 
 let botToken = process.env.DEV_BOT_API_TOKEN;
