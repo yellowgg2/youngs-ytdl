1a24c564e614ba8390a25063c48675a07c575562
Youngki Kang <yellowgg2@gmail.com>
2022-04-12 08:32:36 +0900

    en readme added

--- bot/download-watch.sh
+++ bot/download-watch.sh
@@ -4,5 +4,5 @@ while true
 do
        touch ./lastwatch
        sleep 10
-       find /volume1/Music/bot-download -cnewer ./lastwatch -exec synoindex -R /volume1/Music/bot-download {} \;
+       find $DOWNLOAD_PATH -cnewer ./lastwatch -exec synoindex -R $DOWNLOAD_PATH {} \;
 done
