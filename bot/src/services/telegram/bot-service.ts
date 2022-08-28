32cb743d7c1b08cd91824f999544e43e651dd30e
Youngki Kang <yellowgg2@gmail.com>
2022-08-28 16:00:40 +0900

    환경변수 추가

--- bot/src/services/telegram/bot-service.ts
+++ bot/src/services/telegram/bot-service.ts
@@ -494,8 +494,9 @@ export default class BotService {
         case /\/ff/.test(cmd[0]):
           this.authUserCommand(chatId, username, () => {
             this.sendFileTypeButtons(chatId, this._fileTypeMsg, true);
-            let rootPath = process.env.SEARCH_ROOT_PATH ?? "/volume1/Music";
-            this.runLinuxCommand(`find ${rootPath} -name "*${cmd[1]}*"`);
+            this.runLinuxCommand(
+              `find /ytdlbot/searchroot -name "*${cmd[1]}*"`
+            );
           });
           break;
         case /\/showft/.test(cmd[0]):
